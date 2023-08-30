dea89f15be512d55a1853aa262608fddaa16d768
goldenkingstyle <108811629+goldenkingstyle@users.noreply.github.com>
2023-08-30 03:17:59 +0300

    Update CorsConfiguration.java
    added some fixes

--- game-crash-service/src/main/java/eu/panic/gamecrashservice/configuration/CorsConfiguration.java
+++ game-crash-service/src/main/java/eu/panic/gamecrashservice/configuration/CorsConfiguration.java
@@ -10,25 +10,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        
         registry.addMapping("/api/game/crash/play")
                 .allowedOrigins("http://localhost:80")
                 .allowedMethods("POST")
                 .allowedHeaders("*");
+        
         registry.addMapping("/api/game/crash/take")
                 .allowedOrigins("http://localhost:80")
                 .allowedMethods("POST")
                 .allowedHeaders("*");
+        
         registry.addMapping("/api/game/crash/getAll")
                 .allowedOrigins("http://localhost:80")
                 .allowedMethods("GET")
                 .allowedHeaders("*");
+        
         registry.addMapping("/api/game/crash/getLast")
                 .allowedOrigins("http://localhost:80")
                 .allowedMethods("GET")
                 .allowedHeaders("*");
+        
         registry.addMapping("/api/game/crash/getAllBets")
                 .allowedOrigins("http://localhost:80")
                 .allowedMethods("GET")
                 .allowedHeaders("*");
+        
     }
 }
